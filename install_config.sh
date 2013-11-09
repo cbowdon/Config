1a0a606ed3bf7d3808274fd1d0d3f900ad06112a
Chris Bowdon <c.bowdon@bath.edu>
2013-11-09 22:51:02 +0000

    Added ghci.conf.

--- install_config.sh
+++ install_config.sh
@@ -1,6 +1,6 @@
 function link {
     ln -sfb $(pwd)/$1 ~/$1
-    ls -al ~ | grep $1
+    ls -al ~/$1
 }
 
 # TODO for file in $(ls -la | grep ????)
@@ -9,6 +9,7 @@ link .screenrc
 link .vimrc
 link .emacs
 link .vim/plugin/slime.vim
+link .ghc/ghci.conf
 
 # TODO Merge with .bash_profile / make system for PATH per system
 #link .bashrc
